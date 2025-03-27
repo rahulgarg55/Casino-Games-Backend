5821fa077dba6c6dd07b6bd43917bc791bd6212b
Chetanjot98 <chetanjot.singh@basta-group.com>
2025-03-27 18:04:41 +0530

    27032025 Update api endpoints

--- casino-backend/src/models/stripeConfig.ts
+++ casino-backend/src/models/stripeConfig.ts
@@ -1,17 +1,23 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IStripeConfig extends Document {
   stripeWebhookSecret: string;
   stripeSecretKey: string;
   stripePublishableKey: string;
-  stripeTestMode: boolean;
+  stripeMode: number; // 0 = test , 1= live
 }
 
-const StripeConfigSchema: Schema = new Schema({
-  stripeWebhookSecret: { type: String, required: true },
-  stripeSecretKey: { type: String, required: true },
-  stripePublishableKey: { type: String, required: true },
-  stripeTestMode: { type: Boolean, required: true },
-}, { timestamps: true });
+const StripeConfigSchema: Schema = new Schema(
+  {
+    stripeWebhookSecret: { type: String, required: true },
+    stripeSecretKey: { type: String, required: true },
+    stripePublishableKey: { type: String, required: true },
+    stripeMode: { type: Number, required: true }, // 0 = test , 1= live
+  },
+  { timestamps: true },
+);
 
-export const StripeConfig = mongoose.model<IStripeConfig>("StripeConfig", StripeConfigSchema);
+export const StripeConfig = mongoose.model<IStripeConfig>(
+  'StripeConfig',
+  StripeConfigSchema,
+);
