4ade7b9e593e26c2cf062ce9ea34c8153f9e0179
Chetanjot98 <chetanjot.singh@basta-group.com>
2025-04-25 15:25:11 +0530

    25042025 add multi translation json

--- casino-backend/src/app.ts
+++ casino-backend/src/app.ts
@@ -17,7 +17,7 @@ import path from 'path'
 import { I18n } from  'i18n';
 
 const i18n = new I18n({
-  locales: ['en','mt'],
+  locales: ['en','mt',"pt","id","fil","vi"," ko","th"],
   directory: path.join(__dirname, 'translation'),
   defaultLocale:"en"
 })
